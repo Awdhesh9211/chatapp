af1013d691830aa80b86f8839fe175576903a38a
Awdhesh Gaund <116190087+Awdhesh9211@users.noreply.github.com>
2024-01-24 08:06:46 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCuqKnvc8-PYUtvJRcAdkfM6D_42aSo0rQ",
+  apiKey: "enter your key",
   authDomain: "project-chatapp-bc4af.firebaseapp.com",
   projectId: "project-chatapp-bc4af",
   storageBucket: "project-chatapp-bc4af.appspot.com",
-  messagingSenderId: "644860655209",
-  appId: "1:644860655209:web:266148361e0e7bf28114f8",
-  measurementId: "G-4GYFN0LPHW"
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
